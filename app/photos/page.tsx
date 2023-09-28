6c4022ca584bd85ec650a9e2978a68495fc83a2e
Yuya S <yshimbo@protonmail.com>
2023-09-28 16:54:27 -0600

    added scadcn button

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -1,7 +1,28 @@
+"use client";
+import { CldUploadButton, CldImage } from "next-cloudinary";
+import { Button } from "@/components/ui/button";
+
+type UploadResult = {
+  info: {
+    public_id: string;
+  };
+  event: "success";
+};
+
 export default function Gallery() {
   return (
     <section>
-      <div>Gallery</div>;
+      <div className="flex absolute top-0 right-0 ">
+        <Button asChild>
+          <CldUploadButton
+            onUpload={(result: any) => {
+              console.log(result.info.public_id);
+              // setImageId(result.info.public_id);
+            }}
+            uploadPreset="i8zowa5f"
+          />
+        </Button>
+      </div>
     </section>
   );
 }
