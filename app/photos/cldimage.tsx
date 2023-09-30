595194fc5cbace2e87a5deb4707ef2476eccbe20
Yuya S <yshimbo@protonmail.com>
2023-09-29 21:14:29 -0600

    calling cld API to fetch images

---
+++ app/photos/cldimage.tsx
@@ -0,0 +1,6 @@
+"use client";
+import { CldUploadButton, CldImage } from "next-cloudinary";
+
+export function CLDimage(props: any) {
+  return <CldImage {...props} />;
+}
--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -1,4 +1,6 @@
+import { CLDimage } from "./cldimage";
 import UploadButton from "./uploadbutton";
+import cloudinary from "cloudinary";
 
 type UploadResult = {
   info: {
@@ -6,12 +8,37 @@ type UploadResult = {
   };
   event: "success";
 };
+type SearchResult = {
+  public_id: string;
+};
 
-export default function Gallery() {
+export default async function Gallery() {
+  const results = (await cloudinary.v2.search
+    .expression("resource_type:image")
+    .sort_by("created_at", "desc")
+    .max_results(10)
+    .execute()) as { resources: SearchResult[] };
+
+  console.log(results);
+
   return (
     <section>
-      <div className="flex absolute top-0 right-0 ">
-        <UploadButton />
+      <div className="flex flex-col">
+        <div className="flex justify-between">
+          <h1 className="font-bold ">Welcome</h1>
+          <UploadButton />
+        </div>
+      </div>
+      <div className="grid grid-cols-3">
+        {results.resources.map((result) => (
+          <CLDimage
+            width="400"
+            height="300"
+            key={result.public_id}
+            src={result.public_id}
+            alt="image"
+          />
+        ))}
       </div>
     </section>
   );
